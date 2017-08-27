461bf6a204cfe847873ef449357921b8dd9040f3
sLimba <lieskivi.valtteri@gmail.com>
2017-08-27 15:07:57 +0300

    revert: fef0d63

--- drivers/input/misc/tri_state_key.c
+++ drivers/input/misc/tri_state_key.c
@@ -28,7 +28,6 @@
 
 #define DRV_NAME	"tri-state-key"
 
-<<<<<<< HEAD
 /*
 	        	KEY1(GPIO1)	KEY2(GPIO92)
 pin1 connect to pin4	0	            1         | MUTE
@@ -58,19 +57,6 @@ typedef enum
 //static struct platform_device *tri_state_key_dev;
 >>>>>>> parent of 2b88a468aa68... input: tri-state: rewritten tri-state-key driver
 
-=======
-#define KEYCODE_BASE 600
-#define TOTAL_KEYCODES 6
-
-/*
- * Default mapping between OP's sti-state switch and OPPO's key codes
- * see Constants.java in device/oppo/common
- */
-static int current_mode = 0;
-static int keyCode_slider_top = KEYCODE_BASE + 1;
-static int keyCode_slider_middle = KEYCODE_BASE + 2;
-static int keyCode_slider_bottom = KEYCODE_BASE + 3;
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 
 <<<<<<< HEAD
 struct switch_dev_data {
@@ -90,14 +76,8 @@ struct switch_dev_data
 	int key2_gpio;//key2 gpio77
 	int key3_gpio;
 
-<<<<<<< HEAD
 	struct regulator *vdd_io;
 	//bool power_enabled;
-=======
-	int key1_gpio;
-	int key2_gpio;
-	int key3_gpio;
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 
 	struct work_struct work;
 	struct switch_dev sdev;
@@ -121,7 +101,6 @@ static int set_gpio_by_pinctrl(void)
 //#endif
 static void switch_dev_work(struct work_struct *work)
 {
-<<<<<<< HEAD
 
 	int key1,key2,key3;
 	//pr_err("%s  gpio_get_value(%d)=%d\n",__func__,switch_data->key1_gpio,gpio_get_value(switch_data->key1_gpio));
@@ -143,32 +122,12 @@ static void switch_dev_work(struct work_struct *work)
 	} else if (key3 == 0) {
 		mode = MODE_NORMAL;
 		keyCode = KEY_SLIDER_BOTTOM;
-=======
-	int keyCode;
-	int mode;
-	mutex_lock(&sem);
+	}
 
-	if(!gpio_get_value(switch_data->key3_gpio))
-	{
-		mode = 3;
-		keyCode = keyCode_slider_bottom;
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
-	}
-	else if(!gpio_get_value(switch_data->key2_gpio))
-	{
-		mode = 2;
-		keyCode = keyCode_slider_middle;
-	}
-	else if(!gpio_get_value(switch_data->key1_gpio))
-	{
-		mode = 1;
-		keyCode = keyCode_slider_top;
-	}
-        if (current_mode != mode) {
+	if (current_mode != mode && mode != MODE_UNKNOWN) {
 		current_mode = mode;
 		switch_set_state(&switch_data->sdev, current_mode);
 		send_input(keyCode);
-<<<<<<< HEAD
 		printk(DRV_NAME " changed to mode: %d\n", switch_data->sdev.state);
 =======
 	mutex_lock(&sem);
@@ -202,9 +161,6 @@ static void switch_dev_work(struct work_struct *work)
 
 		printk(KERN_ERR "%s ,tristatekey set to state(%d) \n",__func__,switch_data->sdev.state);
 >>>>>>> parent of 2b88a468aa68... input: tri-state: rewritten tri-state-key driver
-=======
-		printk("%s ,tristate set to state(%d) \n", __func__, switch_data->sdev.state);
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 	}
 	mutex_unlock(&sem);
 }
@@ -268,14 +224,6 @@ static int switch_dev_get_devtree_pdata(struct device *dev)
 	return 0;
 }
 
-<<<<<<< HEAD
-=======
-static struct of_device_id tristate_dev_of_match[] = {
-	{ .compatible = "oneplus,tri-state-key", },
-	{ },
-};
-MODULE_DEVICE_TABLE(of, tristate_dev_of_match);
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 #else
 
 static inline int
@@ -286,7 +234,6 @@ switch_dev_get_devtree_pdata(struct device *dev)
 }
 #endif
 
-<<<<<<< HEAD
 <<<<<<< HEAD
 #define REGISTER_IRQ_FOR(KEY)\
 	switch_data->irq_##KEY = gpio_to_irq(switch_data->KEY##_gpio);\
@@ -312,139 +259,12 @@ switch_dev_get_devtree_pdata(struct device *dev)
 			switch_data->irq_##KEY = -EINVAL;\
 			goto err_request_irq;\
 		}\
-=======
-	// Keycode top
-static int keyCode_top_show(struct seq_file *seq, void *offset)
-{
-    seq_printf(seq, "%d\n", keyCode_slider_top);
-    return 0;
-}
-static ssize_t keyCode_top_write(struct file *file, const char __user *page, size_t t, loff_t *lo)
-{
-	int data;
-	char buf[10];
-
-	if (copy_from_user(buf, page, t))
-	{
-		dev_err(switch_data->dev, "read proc input error.\n");
-		return t;
 	}
-
-	if (sscanf(buf, "%d", &data) != 1)
-		return t;
-	if (data < KEYCODE_BASE || data >= (KEYCODE_BASE + TOTAL_KEYCODES))
-		return t;
-
-	keyCode_slider_top = data;
-	if (current_mode == 1)
-		send_input(keyCode_slider_top);
-
-	return t;
-}
-static int keyCode_top_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, keyCode_top_show, inode->i_private);
-}
-const struct file_operations proc_keyCode_top =
-{
-	.owner		= THIS_MODULE,
-	.open		= keyCode_top_open,
-	.read		= seq_read,
-	.write		= keyCode_top_write,
-	.llseek 	= seq_lseek,
-	.release	= single_release,
-};
-
-	// Keycode middle
-static int keyCode_middle_show(struct seq_file *seq, void *offset)
-{
-    seq_printf(seq, "%d\n", keyCode_slider_middle);
-    return 0;
-}
-static ssize_t keyCode_middle_write(struct file *file, const char __user *page, size_t t, loff_t *lo)
-{
-	int data;
-	char buf[10];
-
-	if (copy_from_user(buf, page, t))
-	{
-		dev_err(switch_data->dev, "read proc input error.\n");
-		return t;
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
-	}
-
-	if (sscanf(buf, "%d", &data) != 1)
-		return t;
-	if (data < KEYCODE_BASE || data >= (KEYCODE_BASE + TOTAL_KEYCODES))
-		return t;
-
-	keyCode_slider_middle = data;
-	if (current_mode == 2)
-		send_input(keyCode_slider_middle);
-
-	return t;
-}
-static int keyCode_middle_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, keyCode_middle_show, inode->i_private);
-}
-const struct file_operations proc_keyCode_middle =
-{
-	.owner		= THIS_MODULE,
-	.open		= keyCode_middle_open,
-	.read		= seq_read,
-	.write		= keyCode_middle_write,
-	.llseek 	= seq_lseek,
-	.release	= single_release,
-};
-
-	// Keycode bottom
-static int keyCode_bottom_show(struct seq_file *seq, void *offset)
-{
-    seq_printf(seq, "%d\n", keyCode_slider_bottom);
-    return 0;
-}
-static ssize_t keyCode_bottom_write(struct file *file, const char __user *page, size_t t, loff_t *lo)
-{
-	int data;
-	char buf[10];
-
-	if (copy_from_user(buf, page, t))
-	{
-		dev_err(switch_data->dev, "read proc input error.\n");
-		return t;
-	}
-
-	if (sscanf(buf, "%d", &data) != 1)
-		return t;
-	if (data < KEYCODE_BASE || data >= (KEYCODE_BASE + TOTAL_KEYCODES))
-		return t;
-
-	keyCode_slider_bottom = data;
-	if (current_mode == 3)
-		send_input(keyCode_slider_bottom);
-
-	return t;
-}
-static int keyCode_bottom_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, keyCode_bottom_show, inode->i_private);
-}
-const struct file_operations proc_keyCode_bottom =
-{
-	.owner		= THIS_MODULE,
-	.open		= keyCode_bottom_open,
-	.read		= seq_read,
-	.write		= keyCode_bottom_write,
-	.llseek 	= seq_lseek,
-	.release	= single_release,
-};
 
 static int tristate_dev_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	int error = 0;
-	int i;
 
 	switch_data = kzalloc(sizeof(struct switch_dev_data), GFP_KERNEL);
 	switch_data->dev = dev;
@@ -462,11 +282,6 @@ static int tristate_dev_probe(struct platform_device *pdev)
 	dev= &pdev->dev;
 	error = 0;
 
-<<<<<<< HEAD
-=======
-	for (i = KEYCODE_BASE; i < KEYCODE_BASE + TOTAL_KEYCODES; i++)
-	    set_bit(i, switch_data->input->keybit);
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 
 <<<<<<< HEAD
 	set_bit(KEY_SLIDER_TOP, switch_data->input->keybit);
@@ -509,7 +324,6 @@ static int tristate_dev_probe(struct platform_device *pdev)
 		goto err_switch_dev_register;
 	}
 
-<<<<<<< HEAD
 	//config irq gpio and request irq
 	switch_data->irq_key1 = gpio_to_irq(switch_data->key1_gpio);
 	if (switch_data->irq_key1 <= 0)
@@ -620,114 +434,6 @@ static int tristate_dev_probe(struct platform_device *pdev)
 
 	}
 
-=======
-	// irqs and work, timer stuffs
-	//config irq gpio and request irq
-	switch_data->irq_key1 = gpio_to_irq(switch_data->key1_gpio);
-       if (switch_data->irq_key1 <= 0)
-       {
-            printk("%s, irq number is not specified, irq #= %d, int pin=%d\n\n", __func__, switch_data->irq_key1, switch_data->key1_gpio);
-            goto err_detect_irq_num_failed;
-       }
-       else
-       {
-        	error = gpio_request(switch_data->key1_gpio,"tristate_key1-int");
-        	if(error < 0)
-        	{
-        		printk(KERN_ERR "%s: gpio_request, err=%d", __func__, error);
-        		goto err_request_gpio;
-        	}
-        	error = gpio_direction_input(switch_data->key1_gpio);
-        	if(error < 0)
-        	{
-        		printk(KERN_ERR "%s: gpio_direction_input, err=%d", __func__, error);
-        		goto err_set_gpio_input;
-        	}
-
-			error = request_irq(switch_data->irq_key1, switch_dev_interrupt,
-			    IRQF_TRIGGER_FALLING, "tristate_key1", switch_data);
-
-        	if (error) {
-        		dev_err(dev,
-        			"request_irq %i failed.\n",
-        			switch_data->irq_key1);
-
-        		switch_data->irq_key1 = -EINVAL;
-        		goto err_request_irq;
-            }
-       }
-
-	 switch_data->irq_key2 = gpio_to_irq(switch_data->key2_gpio);
-       if (switch_data->irq_key2 <= 0)
-       {
-            printk("%s, irq number is not specified, irq #= %d, int pin=%d\n\n", __func__, switch_data->irq_key2, switch_data->key2_gpio);
-            goto err_detect_irq_num_failed;
-       }
-       else
-       {
-        	error = gpio_request(switch_data->key2_gpio,"tristate_key2-int");
-        	if(error < 0)
-        	{
-        		printk(KERN_ERR "%s: gpio_request, err=%d", __func__, error);
-        		goto err_request_gpio;
-        	}
-        	error = gpio_direction_input(switch_data->key2_gpio);
-        	if(error < 0)
-        	{
-        		printk(KERN_ERR "%s: gpio_direction_input, err=%d", __func__, error);
-        		goto err_set_gpio_input;
-        	}
-
-			error = request_irq(switch_data->irq_key2, switch_dev_interrupt,
-			    IRQF_TRIGGER_FALLING, "tristate_key2", switch_data);
-
-        	if (error) {
-        		dev_err(dev,
-        			"request_irq %i failed.\n",
-        			switch_data->irq_key2);
-
-        		switch_data->irq_key2 = -EINVAL;
-        		goto err_request_irq;
-            }
-
-       }
-
-	   switch_data->irq_key3 = gpio_to_irq(switch_data->key3_gpio);
-	   if (switch_data->irq_key3 <= 0)
-	   {
-	            printk("%s, irq number is not specified, irq #= %d, int pin=%d\n\n", __func__, \
-	            switch_data->irq_key3, switch_data->key3_gpio);
-	            goto err_detect_irq_num_failed;
-       }
-       else
-       {
-        	error = gpio_request(switch_data->key3_gpio,"tristate_key3-int");
-        	if(error < 0)
-        	{
-        		printk(KERN_ERR "%s: gpio_request, err=%d", __func__, error);
-        		goto err_request_gpio;
-        	}
-        	error = gpio_direction_input(switch_data->key3_gpio);
-        	if(error < 0)
-        	{
-        		printk(KERN_ERR "%s: gpio_direction_input, err=%d", __func__, error);
-        		goto err_set_gpio_input;
-        	}
-
-			error = request_irq(switch_data->irq_key3, switch_dev_interrupt,
-			    IRQF_TRIGGER_FALLING, "tristate_key3", switch_data);
-
-        	if (error) {
-        		dev_err(dev,
-        			"request_irq %i failed.\n",
-        			switch_data->irq_key3);
-
-        		switch_data->irq_key3 = -EINVAL;
-        		goto err_request_irq;
-            }
-
-       }
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 
 	INIT_WORK(&switch_data->work, switch_dev_work);
 
@@ -785,7 +491,6 @@ static int tristate_dev_remove(struct platform_device *pdev)
 	return 0;
 }
 
-<<<<<<< HEAD
 #ifdef CONFIG_OF
 static struct of_device_id tristate_dev_of_match[] =
 {
@@ -797,9 +502,6 @@ MODULE_DEVICE_TABLE(of, tristate_dev_of_match);
 
 static struct platform_driver tristate_dev_driver =
 {
-=======
-static struct platform_driver tristate_dev_driver = {
->>>>>>> f8e354da7e6a... Merge "op5: tri_state_key: allow for additional settings" into ng7.1
 	.probe	= tristate_dev_probe,
 	.remove	= tristate_dev_remove,
 	.driver	= {
